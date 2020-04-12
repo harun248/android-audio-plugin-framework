04c454506dc07330633a38a92bddf68782e7e905
atsushieno <atsushieno@gmail.com>
2020-04-12 21:25:35 +0900

    [aaphostsample2] rewrote ModalDrawerLayout parts with my own modal panel.

--- java/aaphostsample2/src/main/java/org/androidaudioplugin/aaphostsample2/MainActivity.kt
+++ java/aaphostsample2/src/main/java/org/androidaudioplugin/aaphostsample2/MainActivity.kt
@@ -1,16 +1,18 @@
 package org.androidaudioplugin.aaphostsample2
 
 import android.os.Bundle
+import androidx.animation.AnimatedFloat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.emptyContent
 import androidx.compose.remember
 import androidx.compose.state
-import androidx.ui.core.Modifier
-import androidx.ui.core.setContent
+import androidx.ui.core.*
 import androidx.ui.foundation.*
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
+import androidx.ui.graphics.Paint
+import androidx.ui.graphics.PaintingStyle
 import androidx.ui.layout.*
 import androidx.ui.material.*
 import androidx.ui.material.icons.Icons
@@ -19,6 +21,7 @@ import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import androidx.ui.unit.sp
+import androidx.ui.unit.toRect
 import org.androidaudioplugin.PluginInformation
 
 class MainActivity : AppCompatActivity() {
@@ -41,17 +44,17 @@ fun AAPHostSample2App() {
 @Composable
 fun AAPHostSample2AppContent() {
 
-    var (state, onStateChange) = state { DrawerState.Closed }
+    var (state, onStateChange) = state { PluginListPanelState.None }
     Surface {
-        ModalDrawerLayout(
-            drawerState = state,
+        ModalPluginListPanelLayout(
+            visibilityState = state,
             onStateChange = onStateChange,
-            drawerContent = {
+            panelContent = {
                 VerticalScroller {
                     Column {
                         AvailablePlugins(onItemClick = { plugin ->
                             AAPHostSampleState.pluginGraph.nodes.add(AAPPluginGraphNode(plugin))
-                            onStateChange(DrawerState.Closed)
+                            onStateChange(PluginListPanelState.None)
                         })
                     }
                 }
@@ -62,9 +65,64 @@ fun AAPHostSample2AppContent() {
     }
 }
 
+enum class PluginListPanelState {
+    None,
+    Visible
+}
 
 @Composable
-fun HomeScreen(onStateChange: (DrawerState) -> Unit, scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
+fun ModalPluginListPanelLayout(
+    visibilityState: PluginListPanelState,
+    onStateChange: (PluginListPanelState) -> Unit,
+    panelContent: @Composable() () -> Unit,
+    bodyContent: @Composable() () -> Unit
+) {
+    Box(Modifier.fillMaxSize()) {
+        Stack {
+            bodyContent()
+            if (visibilityState == PluginListPanelState.Visible) {
+                Scrim(visibilityState, onStateChange)
+                Box(
+                    padding = PluginListPanelPadding
+                ) {
+                    // remove Container when we will support multiply children
+                    Surface {
+                        Box(Modifier.fillMaxSize(), children = panelContent)
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Taken from Drawer.kt in androidx.ui/ui-material and modified.  begin
+private const val ScrimDefaultOpacity = 0.32f
+private val PluginListPanelPadding = 36.dp
+
+@Composable
+private fun Scrim(
+    state: PluginListPanelState,
+    onStateChange: (PluginListPanelState) -> Unit // FIXME: change from DrawerState to something else (PluginListState?)
+) {
+    val scrimContent = @Composable {
+        val paint = remember { Paint().apply { style = PaintingStyle.fill } }
+        val color = MaterialTheme.colors.onSurface
+        Canvas(Modifier.fillMaxSize()) {
+            val scrimAlpha = ScrimDefaultOpacity
+            paint.color = color.copy(alpha = scrimAlpha)
+            drawRect(size.toRect(), paint)
+        }
+    }
+    if (state == PluginListPanelState.Visible) {
+        Clickable(onClick = { onStateChange(PluginListPanelState.None) }, children = scrimContent)
+    } else {
+        scrimContent()
+    }
+}
+// end
+
+@Composable
+fun HomeScreen(onStateChange: (PluginListPanelState) -> Unit, scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
     Scaffold(
         scaffoldState = scaffoldState,
         topAppBar = {
@@ -103,8 +161,8 @@ fun HomeScreen(onStateChange: (DrawerState) -> Unit, scaffoldState: ScaffoldStat
 }
 
 @Composable
-fun Rack(onPluginListDrawerStateChange: (DrawerState) -> Unit) {
-    Button(onClick = { onPluginListDrawerStateChange(DrawerState.Opened) }) {
+fun Rack(onPluginListDrawerStateChange: (PluginListPanelState) -> Unit) {
+    Button(onClick = { onPluginListDrawerStateChange(PluginListPanelState.Visible) }) {
         Text("Add")
     }
     RackConnections()
